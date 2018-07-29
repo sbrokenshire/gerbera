e62507221df7b2cfcb8461dfb5f06ad6162566e8
Steve Brokenshire <sbrokenshire@xestia.co.uk>
2018-07-29 15:36:39 +0100

    Modified make_server_icons.sh script
    - Images are resized to their exact sizes even if an image like
      artwork/logo.png is not square (464x477) in size
    - Respects the existing image background colours within commit
      398994f

--- scripts/make_server_icons.sh
+++ scripts/make_server_icons.sh
@@ -7,14 +7,18 @@
 src="${1}"
 
 
-convert "${src}" -filter Lanczos -resize x32 -background black -flatten bmp:"mt-icon32.bmp"
-convert "${src}" -filter Lanczos -resize x32 -background black -flatten png:"mt-icon32.png"
-convert "${src}" -filter Lanczos -resize x32 -background black -flatten jpg:"mt-icon32.jpg"
+convert "${src}" -filter Lanczos -resize 32x32\! -background none -flatten bmp:"mt-icon32.bmp"
+convert "${src}" -filter Lanczos -resize 32x32\! -background transparent -flatten png:"mt-icon32.png"
+convert "${src}" -filter Lanczos -resize 32x32\! -background white -flatten jpg:"mt-icon32.jpg"
 
-convert "${src}" -filter Lanczos -resize x48 -background black -flatten bmp:"mt-icon48.bmp"
-convert "${src}" -filter Lanczos -resize x48 -background black -flatten png:"mt-icon48.png"
-convert "${src}" -filter Lanczos -resize x48 -background black -flatten jpg:"mt-icon48.jpg"
+convert "${src}" -filter Lanczos -resize 48x48\! -background white -flatten bmp:"mt-icon48.bmp"
+convert "${src}" -filter Lanczos -resize 48x48\! -background transparent -flatten png:"mt-icon48.png"
+convert "${src}" -filter Lanczos -resize 48x48\! -background white -flatten jpg:"mt-icon48.jpg"
 
-convert "${src}" -filter Lanczos -resize x120 -background black -flatten bmp:"mt-icon120.bmp"
-convert "${src}" -filter Lanczos -resize x120 -background black -flatten png:"mt-icon120.png"
-convert "${src}" -filter Lanczos -resize x120 -background black -flatten jpg:"mt-icon120.jpg"
+convert "${src}" -filter Lanczos -resize 120x120\! -background white -flatten bmp:"mt-icon120.bmp"
+convert "${src}" -filter Lanczos -resize 120x120\! -background transparent -flatten png:"mt-icon120.png"
+convert "${src}" -filter Lanczos -resize 120x120\! -background white -flatten jpg:"mt-icon120.jpg"
+
+convert "${src}" -filter Lanczos -resize 260x260\! -background white -flatten bmp:"mt-icon260.bmp"
+convert "${src}" -filter Lanczos -resize 260x260\! -background transparent -flatten png:"mt-icon260.png"
+convert "${src}" -filter Lanczos -resize 260x260\! -background white -flatten jpg:"mt-icon260.jpg"
